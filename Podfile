5002e186c46cfa98e6bc9f85838dd3f4732c0858
qujie <793116483@qq.com>
2017-08-21 14:09:24 +0800

    Pod ReactiveObjC 框架

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+use_frameworks!
+
+target 'ReactiveCocoaDome' do
+
+    pod 'AFNetworking', '~> 3.0.4'
+    pod 'ReactiveObjC', '~> 3.0.0'
+
+end
--- ReactiveCocoaDome/ViewController.m
+++ ReactiveCocoaDome/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import <ReactiveObjC/ReactiveObjC.h>
+
 @interface ViewController ()
 
 @end
@@ -18,6 +20,28 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    RACSignal * signal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        
+        NSLog(@"信号 block");
+
+        return [RACDisposable disposableWithBlock:^{
+            NSLog(@"disposableWithBlock");
+        }];
+    }];
+    
+    [signal subscribeNext:^(id  _Nullable x) {
+        
+        NSLog(@"订阅 block = %@",x);
+        
+    } error:^(NSError * _Nullable error) {
+        
+        NSLog(@"订阅错误 = %@",error);
+        
+    } completed:^{
+        
+        NSLog(@"完成订阅");
+        
+    }];
     
 }
 
